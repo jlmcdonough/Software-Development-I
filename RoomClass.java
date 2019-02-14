082c3277f1139f7f424e15e94208d1a38e83cda8
pamcnamara17 <47303643+pamcnamara17@users.noreply.github.com>
2019-02-14 17:36:13 -0500

    RoomClass.java

---
+++ RoomClass.java
@@ -0,0 +1,19 @@
+//Patrick McNamara and Joseph McDonough
+
+public class RoomClass {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Room [] Location = new Room [8];
+		Location [0] = new Room(0, "Laboratory", "Welcome to the Laboratory!");
+		Location [1] = new Room(1, "Classroom", "Welcome to the Classroom!");
+		Location [2] = new Room(2, "Gym", "Welcome to the Gym!");
+		Location [3] = new Room(3, "Magic Shop", "Welcome to the Magic Shop!");
+		Location [4] = new Room(4, "Nurse", "Welcome to the Nurse's Office!");
+		Location [5] = new Room(5, "Office", "Welcome to the main Office!");
+		Location [6] = new Room(6, "Cafe", "Welcome to the Cafeteria!");
+		Location [7] = new Room(7, "Auditorium", "Welcome to the Auditorium!");
+	}
+
+}
